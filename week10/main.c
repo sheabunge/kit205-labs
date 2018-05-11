3d1883c22dd7a11496c013d562510af79a3125d3
Shea Bunge <bungeshea@gmail.com>
2018-05-11 17:32:43 +1000

    Add provided method for reference

--- week10/main.c
+++ week10/main.c
@@ -1,6 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+float knapsackValueSolution(int w[], int v[], int W, int n) {
+	// make 2D array (array of arrays)
+	int **A = malloc(n * sizeof *A); // or malloc(n * sizeof(int*))
+	for (int i = 0; i < n; i++) {
+		A[i] = malloc(W * sizeof *A[i]); // or malloc(n * sizeof(int))
+
+		// the following initialisation is not necessary for this problem
+		for (int j = 0; j < W; j++) {
+			A[i][j] = 0;
+		}
+	}
+
+	for (int i = 0; i < n; i++) { // column (item number)
+		for (int j = 0; j < W; j++) { // row (knapsack weight)
+			if (w[i] <= j) { // item i would fit in the knapsack
+				if (i == 0) { // first item so take it
+					A[i][j] = v[i];
+				} else {
+					if (A[i - 1][j - w[i]] + v[i] > A[i - 1][j]) { // better to take this item
+						A[i][j] = A[i - 1][j - w[i]] + v[i];
+					} else { // better to leave this item
+						A[i][j] = A[i - 1][j];
+					}
+				}
+			} else { // doesn't fit
+				if (i == 0) { // nothing fits yet
+					A[i][j] = 0;
+				} else { // this item doesn't fit so stick with previous best
+					A[i][j] = A[i - 1][j];
+				}
+			}
+		}
+	}
+
+	int result = A[n - 1][W - 1]; // remember this so we can free memory
+
+	// free inner arrays first
+	for (int i = 0; i < n; i++) {
+		free(A[i]);
+	}
+	// then free array of arrays
+	free(A);
+
+	return result;
+}
+
+
 int knapsackValue(const int weights[], const int values[], int const capacity, const int items) {
 
 	int **table = malloc(sizeof(int *) * (items + 1));
